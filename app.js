e0437d9530e26c250781ccc5d3cce543190dc889
Your Name <sahil.savaliya@simformsolutions.com>
2023-06-21 12:48:26 +0530

    phase1 completed

---
+++ app.js
@@ -0,0 +1,50 @@
+/* eslint-disable no-console */
+// process.env.TZ = "UTC";
+const shopSchedule = require("./shop_schedule.json");
+
+const isShopOpen = (day) => {
+  const currentTime = new Date("2023-06-23T14:55:00");
+  const currentDay = currentTime.toLocaleString("en-US", {
+    weekday: "short",
+    hour12: true,
+    hour: "numeric",
+    minute: "numeric",
+  });
+  console.log(currentDay);
+
+  if (currentDay !== day) {
+    return false;
+  }
+
+  const openTime = new Date();
+  const closeTime = new Date();
+
+  shopSchedule.forEach((entry) => {
+    if (entry.day === day) {
+      const [openHour, openMinutes] = entry.open.split(":");
+      const [closeHour, closeMinutes] = entry.close.split(":");
+
+      openTime.setHours(Number(openHour), Number(openMinutes), 0);
+      closeTime.setHours(Number(closeHour), Number(closeMinutes), 0);
+    }
+  });
+
+  return currentTime >= openTime && currentTime <= closeTime;
+};
+
+const getShopStatus = () => {
+  const currentTime = new Date("2023-06-23T14:55:00");
+  // console.log("---------", currentTime);
+  const currentDay = currentTime.toLocaleString("en-US", {
+    weekday: "short",
+    hour12: true,
+    hour: "numeric",
+    minute: "numeric",
+  });
+  console.log(currentDay);
+  const isOpen = isShopOpen(currentDay);
+
+  return isOpen ? "Open!" : "Closed!";
+};
+
+console.log("Shop Status:", getShopStatus());
